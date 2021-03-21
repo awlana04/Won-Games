1c05cda64c6248982019b5526f21cc1db4b701ff
awlana04 <cawlana040@gmail.com>
2021-03-21 18:44:46 -0300

    global styles

---
+++ web/src/components/Main/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.main`
+  width: 100%;
+  height: 100%;
+
+  background-color: #06092B;
+  color: #fff;
+`;
+
+export const Logo = styled.img``;
+
+export const Title = styled.h1``;
+
+export const Description = styled.h2``;

5d714e8ffab18ea4fc5e4e66cc1ade8b34fb61c1
awlana04 <cawlana040@gmail.com>
2021-03-22 23:23:04 -0300

    admin configure themes

---
+++ api/admin/src/components/LeftMenu/LeftMenuLinkSection/LeftMenuListSection.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const LeftMenuListSection = styled.div`
+  max-height: 180px;
+  margin-bottom: 19px;
+  overflow: auto;
+`;
+
+export default LeftMenuListSection;

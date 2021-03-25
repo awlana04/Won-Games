bb59868ff76a0b16365f9dd3322a84fe320e019a
awlana04 <cawlana040@gmail.com>
2021-03-25 17:14:54 -0300

    prismic

--- api/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '84292bcd0e15b4e9be475edf5f31933d'),
-    },
-  },
-});
--- api/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || 'fec79bf1-98e8-4b3b-8b12-9a3ba8100287'
-};
